7ce46580513967df9de0a4e8f527e75559507961
Chahit <uppalchahit@gmail.com>
2024-05-20 16:00:20 -0400

    right works, minor issues with bottle and top can

---
+++ fixed_left_matrices.m
@@ -0,0 +1,9 @@
+translation_matrix = [-0.36 0 0.25
+                        -0.525 0.04 0.07
+                        -0.5 0.4 0.15
+                        -0.617 0.4 0.07];
+rotation_matrix = [pi -pi 0;
+                    pi -pi 0;
+                    pi -pi 0;
+                    pi -pi 0];
+object_matrix = ["can_grip", "bottle_grip", "can_grip", "bottle_grip"];
